81574de21f7466f8bb86d074bc6e1561229d50a6
Samir Iyer <iyersamir@gmail.com>
2025-05-29 15:01:26 +0200

    API keys leak

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgsw0XV_1MlIhv1whgbJuu3s0mBUqApEA",
-  authDomain: "homecookedlive.firebaseapp.com",
-  projectId: "homecookedlive",
-  storageBucket: "homecookedlive.appspot.com",
-  messagingSenderId: "537991850995",
-  appId: "1:537991850995:web:09d489fa108fec79178086",
-  measurementId: "G-V5GSY5WZSM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
